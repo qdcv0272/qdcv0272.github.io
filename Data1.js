d93209614d552b5e3bb3f9b3e29419bd446f4cba
qdcv0272 <qdcv0272@naver.com>
2022-11-16 17:32:54 +0900

    ‘Data’

---
+++ Data1.js
@@ -0,0 +1,22 @@
+[
+  {
+    id: 4,
+    title: "Watch",
+    content: "Smart Watch",
+    price: 120000,
+  },
+
+  {
+    id: 5,
+    title: "Air Pods Max",
+    content: "Headset",
+    price: 110000,
+  },
+
+  {
+    id: 6,
+    title: "Air Pods",
+    content: "Earphone",
+    price: 130000,
+  },
+];
---
+++ Data2.js
@@ -0,0 +1,22 @@
+[
+  {
+    id: 4,
+    title: "Tv",
+    content: "Smart Tv",
+    price: 220000,
+  },
+
+  {
+    id: 5,
+    title: "Air Tag",
+    content: "Gps",
+    price: 310000,
+  },
+
+  {
+    id: 6,
+    title: "Safe",
+    content: "Charger",
+    price: 430000,
+  },
+];
